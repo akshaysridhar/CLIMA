bfc6bb8d4e586ced4e4cab800ae121934ce62f11
Akshay Sridhar <asridhar@caltech.edu>
2019-04-21 16:44:22 -0700

    update rtb driver with periodic domain

--- src/DGmethods/test/Euler/rising_thermal_bubble.jl
+++ src/DGmethods/test/Euler/rising_thermal_bubble.jl
@@ -46,38 +46,39 @@ end
 end
 
 const _nauxstate = 9
-const _a_ϕ, _a_ϕx, _a_ϕy, _a_ϕz, _a_x, _a_y, _a_z ,_a_xmax, _a_ymax = 1:_nauxstate
-@inline function auxiliary_state_initialization!(aux, x, y, z, xmax=brickrange[1][end], ymax = brickrange[2][end])
+const _a_ϕ, _a_ϕx, _a_ϕy, _a_ϕz, _a_x, _a_y, _a_z = 1:_nauxstate
+@inline function auxiliary_state_initialization!(aux, x, y, z) 
   @inbounds begin
     aux[_a_ϕ] = hypot(x, y, z)
     aux[_a_x] = x
     aux[_a_y] = y
     aux[_a_z] = z
-    aux[_a_xmax] = 5 
-    aux[_a_ymax] = 5
     #FIXME 3D compatibility
   end
 end
 
 @inline function rayleigh_sponge!(S, Q, aux, t)
   @inbounds begin
-    x,y,z,xmax,ymax = aux[_a_x], aux[_a_y], aux[_a_z], aux[_a_xmax], aux[_a_ymax]
+    x,y,z= aux[_a_x], aux[_a_y], aux[_a_z]
     # damping parameters 
     # Rayleigh damping (linear relaxation to reference state)
     # details of the technique are provided in 
     # Durran and Klemp (1983): https://doi.org/10.1175/1520-0493(1983)111<2341:ACMFTS>2.0.CO;2    
     # user defined extents of sponge region. currently sponge is inactive on bottom wall
-    α_top       = 1.0
-    α_lateral   = 0.8
-    xmin        = 0
+    # Sponge function in this instance is given by the `squircle` shape
+    U           = Q[_U]
+    V           = Q[_V]
+    α           = 1.0
+    xmin        = -500
+    xmax        = 500
+    ymax        = 1000
     xc          = (xmax + xmin) / 2
     yc          = 0
-    x_rad       = xmax
-    y_rad       = 0.80 * ymax
-    r_actual    = ((x-xc)^4/x_rad^4 + (y-yc)^4/y_rad^4) ^ (1/4)
+    r_actual    = ((x-xc)^4+ (y-yc)^4)^ (1/4)
+    r_sponge    = 0.60 * ymax
     # assign absorptive condition on velocity components
-    S[_U] = - α * sinpi((x-xsponge)/(xsponge))^4 * U
-    S[_V] = - α * sinpi((y-ysponge)/(ysponge))^4 * U
+    S[_U] = - α * sinpi((r_actual-r_sponge)/(r_sponge))^4 * U
+    S[_V] = - α * sinpi((r_actual-r_sponge)/(r_sponge))^4 * V
   
   end
 end
@@ -89,7 +90,6 @@ eulerflux!(F, Q, aux, t, preflux(Q)...)
 @inline function eulerflux!(F, Q, aux, t, P, u, v, w, ρinv)
   @inbounds begin
     ρ, U, V, W, E = Q[_ρ], Q[_U], Q[_V], Q[_W], Q[_E]
-
     F[1, _ρ], F[2, _ρ], F[3, _ρ] = U          , V          , W
     F[1, _U], F[2, _U], F[3, _U] = u * U  + P , v * U      , w * U
     F[1, _V], F[2, _V], F[3, _V] = u * V      , v * V + P  , w * V
@@ -111,50 +111,40 @@ function rising_thermal_bubble!(Q,
   c_p::DFloat           = cp_d
   c_v::DFloat           = cv_d
   p0::DFloat            = MSLP
-  
   gravity::DFloat       = grav
-  
   # initialise with dry domain 
   q_tot::DFloat         = 0 
   q_liq::DFloat         = 0
   q_ice::DFloat         = 0 
-  
   # perturbation parameters for rising bubble
-  r                     = sqrt((x-500)^2 + (y-350)^2)
+  r                     = sqrt((x)^2 + (y-350)^2)
   rc::DFloat            = 250
   θ_ref::DFloat         = 300
   θ_c::DFloat           = 5.0
   Δθ::DFloat            = 0.0
-
   if r <= rc 
     Δθ = θ_c * (1 + cospi(r/rc))/2
   end
-  
   θ                     = θ_ref + Δθ
-  π_exner               = 1 - gravity / (c_p * θ) * y
+  π_exner               = 1.0 - gravity / (c_p * θ) * y
   ρ                     = p0 / (R_gas * θ) * (π_exner)^ (c_v / R_gas)
-  P                     = p0 / (R_gas * θ) * (π_exner) ^ (c_p / c_v)
+  P                     = p0 * (R_gas * (ρ * θ) / p0) ^(c_p/c_v)
   T                     = P / (ρ * R_gas)
-  
   U, V, W               = 0.0 , 0.0 , 0.0 
   # energy definitions
   e_kin                 = (U^2 + V^2 + W^2) / (2*ρ)/ ρ
   e_pot                 = gravity * y
   e_int                 = internal_energy(T, q_tot, q_liq, q_ice)
   E                     = ρ * total_energy(e_kin, e_pot, T, q_tot, q_liq, q_ice)
+  @show(E)
   
   @inbounds Q[_ρ], Q[_U], Q[_V], Q[_W], Q[_E] = ρ, U, V, W, E
 
 end
 
-# initial condition
-const halfperiod = 5
-
 function main(mpicomm, DFloat, topl::AbstractTopology{dim}, N, timeend,
-              ArrayType, dt, brickrange) where {dim}
+              ArrayType, dt) where {dim}
   
-  brickrange=brickrange
-
   grid = DiscontinuousSpectralElementGrid(topl,
                                           FloatType = DFloat,
                                           DeviceArray = ArrayType,
@@ -200,10 +190,6 @@ function main(mpicomm, DFloat, topl::AbstractTopology{dim}, N, timeend,
     end
   end
 
-  #= Paraview calculators:
-  P = (0.4) * (E  - (U^2 + V^2 + W^2) / (2*ρ) - 9.81 * ρ * coordsZ)
-  theta = (100000/287.0024093890231) * (P / 100000)^(1/1.4) / ρ
-  =#
   step = [0]
   mkpath("vtk")
   cbvtk = GenericCallbacks.EveryXSimulationSteps(100) do (init=false)
@@ -244,91 +230,26 @@ end
 function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
   ArrayType = Array
  
-  brickrange = (range(DFloat(0); length=Ne[1]+1, stop=1000),
-                range(DFloat(0); length=Ne[2]+1, stop=1000))
+  xmin,xmax = -500,500
+  ymin,ymax = -600,600
+  brickrange = (range(DFloat(xmin); length=Ne[1]+1, stop=xmax),
+                range(DFloat(ymin); length=Ne[2]+1, stop=ymax))
 
-  topl = BrickTopology(mpicomm, brickrange, periodicity=ntuple(j->true, dim))
-  main(mpicomm, DFloat, topl, N, timeend, ArrayType, dt, brickrange)
+  topl = BrickTopology(mpicomm, brickrange, periodicity=(true,true))
+  main(mpicomm, DFloat, topl, N, timeend, ArrayType, dt)
 
 end
-
-using Test
 let
   MPI.Initialized() || MPI.Init()
   Sys.iswindows() || (isinteractive() && MPI.finalize_atexit())
-  mpicomm = MPI.COMM_WORLD
-  if MPI.Comm_rank(mpicomm) == 0
-    ll = uppercase(get(ENV, "JULIA_LOG_LEVEL", "INFO"))
-    loglevel = ll == "DEBUG" ? Logging.Debug :
-    ll == "WARN"  ? Logging.Warn  :
-    ll == "ERROR" ? Logging.Error : Logging.Info
-    global_logger(ConsoleLogger(stderr, loglevel))
-  else
-    global_logger(NullLogger())
-  end
-
-  if integration_testing
-    timeend = 1
-    numelem = (5, 5, 1)
-
-    polynomialorder = 4
-
-    expected_error = Array{Float64}(undef, 2, 3) # dim-1, lvl
-    expected_error[1,1] = 5.5175136745319797e-01
-    expected_error[1,2] = 6.7757089928958958e-02
-    expected_error[1,3] = 3.2832015676432292e-03
-    expected_error[2,1] = 1.7613313745738965e+00
-    expected_error[2,2] = 2.1526080821361515e-01
-    expected_error[2,3] = 1.0251374591125394e-02
-    lvls = size(expected_error, 2)
-
-    for DFloat in (Float64,) #Float32)
-      for dim = 2:3
-        err = zeros(DFloat, lvls)
-        for l = 1:lvls
-          Ne = ntuple(j->2^(l-1) * numelem[j], dim)
-          dt = 1e-2 / Ne[1]
-          nsteps = ceil(Int64, timeend / dt)
-          dt = timeend / nsteps
-          err[l] = run(mpicomm, dim, Ne, polynomialorder, timeend, DFloat, dt)
-          @test err[l] ≈ DFloat(expected_error[dim-1, l])
-        end
-        @info begin
-          msg = ""
-          for l = 1:lvls-1
-            rate = log2(err[l]) - log2(err[l+1])
-            msg *= @sprintf("\n  rate for level %d = %e\n", l, rate)
-          end
-          msg
-        end
-      end
-    end
-  else
-    numelem = (3, 4, 5)
-    dt = 1e-3
-    timeend = 2dt
-
-    polynomialorder = 4
-
-    mpicomm = MPI.COMM_WORLD
-
-    check_engf_eng0 = Dict{Tuple{Int64, Int64, DataType}, AbstractFloat}()
-    check_engf_eng0[2, 1, Float64] = 9.9999808508887378e-01
-    check_engf_eng0[3, 1, Float64] = 9.9999644038110480e-01
-    check_engf_eng0[2, 3, Float64] = 9.9999878540546705e-01
-    check_engf_eng0[3, 3, Float64] = 9.9999657187253710e-01
-
-    for DFloat in (Float64,) #Float32)
-      for dim = 2:3
-        Random.seed!(0)
-        engf_eng0 = run(mpicomm, dim, numelem[1:dim], polynomialorder, timeend,
-                        DFloat, dt)
-        @test check_engf_eng0[dim, MPI.Comm_size(mpicomm), DFloat] ≈ engf_eng0
-      end
-    end
-  end
+  mpicomm       = MPI.COMM_WORLD
+  timeend       = 700
+  dt            = 1e-2
+  Ne            = (10,10)
+  N             = 4
+  dim           = 2
+  DFloat        = Float64
+  run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
+  isinteractive() || MPI.Finalize()
 end
-
-isinteractive() || MPI.Finalize()
-
 nothing
