19364708bc3d4c4e6e4e02842c418de34357bad6
asraero <asridhar@caltech.edu>
2019-07-29 08:03:21 -0700

    modified:   run-gpu.sh

--- test/DGmethods/compressible_Navier_Stokes/run-gpu.sh
+++ test/DGmethods/compressible_Navier_Stokes/run-gpu.sh
@@ -1,13 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
 
-#BATCH --job-name=rad-test-ld
+#SBATCH --job-name=vreman
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --tasks-per-node=1
-#SBATCH --time=04:00:00
-#SBATCH --mail-user=asridhar@caltech.edu
-#SBATCH --mail-type=ALL
-#SBATCH --output=large-domainSmag.txt
-set -euo pipefail # kill the job if anything fails  
-/home/asridhar/julia-1.3/bin/julia --project=/home/asridhar/CLIMA/env/gpu -e "using Pkg; Pkg.instantiate(); Pkg.API.precompile()"
-mpiexec /home/asridhar/julia-1.3/bin/julia --project=/home/asridhar/CLIMA/env/gpu /home/asridhar/CLIMA/test/DGmethods/compressible_Navier_Stokes/test-dycoms-visc-2d.jl
+#SBATCH --time=6:00:00                                                                                                        
+
+#SBATCH --mail-user=asridhar@caltech.edu                                                                                        
+#SBATCH --mail-type=ALL                                                                                                        
+#SBATCH --output=output-DRF01.txt
+
+set -euo pipefail # kill the job if anything fails                                                                             
+
+/home/asridhar/julia-1.3/bin/julia --project=/home/asridhar/CLIMA/env/gpu -e 'using Pkg; Pkg.instantiate(); Pkg.API.precompile()'
+
+mpirun /home/asridhar/julia-1.3/bin/julia --project=/home/asridhar/CLIMA/env/gpu /home/asridhar/CLIMA/test/DGmethods/compressible_Navier_Stokes/DYCOMS.jl
