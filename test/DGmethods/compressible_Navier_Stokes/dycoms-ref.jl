ecfbfca075c0526260479b86863f34560854bdea
asraero <asridhar@caltech.edu>
2019-08-05 10:41:26 -0700

    1) Equations updated to match overleaf docs 2) Using first-node kernel to access first-interior values 3) First-interior values stored in auxstate

---
+++ test/DGmethods/compressible_Navier_Stokes/dycoms-ref.jl
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=strato-bc1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --tasks-per-node=1
+#SBATCH --time=6:00:00                                                                                                        
+
+#SBATCH --mail-user=asridhar@caltech.edu                                                                                        
+#SBATCH --mail-type=ALL                                                                                                        
+#SBATCH --output=strato-test.out                                                                                        
+
+set -euo pipefail # kill the job if anything fails                                                                             
+
+/home/asridhar/julia-1.3/bin/julia --project=/home/asridhar/CLIMA/env/gpu -e 'using Pkg; Pkg.instantiate(); Pkg.API.precompile()'
+
+mpirun /home/asridhar/julia-1.3/bin/julia --project=/home/asridhar/CLIMA/env/gpu /home/asridhar/CLIMA/test/DGmethods/compressible_Navier_Stokes/dycoms-ref.jl
