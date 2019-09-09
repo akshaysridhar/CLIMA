62ece885ea19297e8c3b85cc8bce52c45933e95e
asraero <asridhar@caltech.edu>
2019-09-09 10:42:59 -0700

    modified:   test/DGmethods/compressible_Navier_Stokes/rising_bubble-model.jl enabled float32 test

--- test/DGmethods/compressible_Navier_Stokes/rising_bubble-model.jl
+++ test/DGmethods/compressible_Navier_Stokes/rising_bubble-model.jl
@@ -191,7 +191,7 @@ let
       device!(MPI.Comm_rank(mpicomm) % length(devices()))
   end
   @testset "$(@__FILE__)" for ArrayType in ArrayTypes
-    FloatType = (Float64,) #Float64)
+    FloatType = (Float32, Float64)
     for DF in FloatType
       brickrange = (range(DF(xmin); length=Ne[1]+1, stop=xmax),
                     range(DF(ymin); length=Ne[2]+1, stop=ymax),
