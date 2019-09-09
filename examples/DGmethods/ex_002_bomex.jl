7a4f94423ff8cf7356a3102ba6bf288e2af7f4c0
asraero <asridhar@caltech.edu>
2019-09-09 10:32:34 -0700

    1) WIP: BOMEX initial condition added 2) VTK field directory name changed 3) TODO: boundaryconditions

--- examples/DGmethods/ex_002_bomex.jl
+++ examples/DGmethods/ex_002_bomex.jl
@@ -42,14 +42,57 @@ end
 """
 function Initialise_BOMEX!(state::Vars, aux::Vars, (x,y,z), t)
   DT         = eltype(state)
-  xvert::DT  = z
-  #Incomplete Initial Condition TODO
-  u, v, w     = DT(7), DT(-5.5), DT(0)
+  # Observed ground quantities
+  pg::DT     = 1.015e5
+  Tg::DT     = 300.4
+  q_totg::DT = 0.02245 # Mixing ratio -> Convert to q_tot 
+  u::DT = 0
+  v::DT = 0
+  w::DT = 0 
+  # Prescribed heights for piece-wise profile construction
+  zl1::DT = 520
+  zl2::DT = 1480
+  zl3::DT = 2000
+  zl4::DT = 3000
+  # Assign piecewise quantities to θ_liq and q_tot 
+  θ_liq::DT = 0 
+  q_tot::DT = 0 
+  q_liq::DT = 0
+  q_ice::DT = 0 
+
+  if z <= zl1
+    θ_liq = 298.7
+    q_tot = 17.0 + (z/zl1)*(16.3-17.0)
+  elseif z > zl1 && z <= zl2
+    θ_liq= 298.7 + (z-zl1) * (302.4-298.7)/(zl2-zl1)
+    q_tot= 16.3 + (z-zl1) * (10.7-16.3)/(zl2-zl1)
+  elseif z > zl2 && z <= zl3
+    θ_liq= 302.4 + (z-zl2) * (308.2-302.4)/(zl3-zl2)
+    q_tot= 10.7 + (z-zl2) * (4.2-10.7)/(zl3-zl2)
+  else 
+    θ_liq= 308.2 + (z-zl3) * (311.85-308.2)/(zl4-zl3)
+    q_tot= 4.2 + (z-zl3) * (3.0-4.2)/(zl4-zl3)
+  end
+  
+  # Set velocity profiles - piecewise profile for u
+  zlv::DT = 700
+  if z <= zlv
+    u = -8.75
+  else
+    u = -8.75 + (z - zlv) * (-4.61 + 8.75)/(zl4 - zlv)
+  end
+  
+  q_tot *= 1e-3 # Convert to kg/kg
+  p = pg * exp(-z/3000) # TODO fix hardcoded maximum height  
+  TS = LiquidIcePotTempSHumEquil_no_ρ(θ_liq, q_tot, p)
+  T = air_temperature(TS)
+  ρ = p / gas_constant_air(TS) / T
+  q_pt = PhasePartition(TS)
   U           = ρ * u
   V           = ρ * v
   W           = ρ * w
   e_kin       = DT(1//2) * (u^2 + v^2 + w^2)
-  e_pot       = grav * xvert
+  e_pot       = DT(grav) * z
   E           = ρ * total_energy(e_kin, e_pot, T, q_pt)
   state.ρ     = ρ
   state.ρu    = SVector(U, V, W) 
@@ -75,7 +118,8 @@ function run(mpicomm, ArrayType, dim, topl, N, timeend, DT, dt, C_smag, LHF, SHF
                       RayleighSponge{DT}(zmax, zsponge, 1), 
                       Subsidence(), 
                       GeostrophicForcing{DT}(7.62e-5, 7, -5.5)), 
-                     BOMEX_BC{DT}(C_drag, LHF, SHF),
+                     #BOMEX_BC{DT}(C_drag, LHF, SHF),
+                      NoFluxBC(),
                      Initialise_BOMEX!)
 
   dg = DGModel(model,
@@ -112,9 +156,9 @@ function run(mpicomm, ArrayType, dim, topl, N, timeend, DT, dt, C_smag, LHF, SHF
   end
 
   step = [0]
-    cbvtk = GenericCallbacks.EveryXSimulationSteps(5000) do (init=false)
-    mkpath("./vtk-dycoms/")
-    outprefix = @sprintf("./vtk-dycoms/dycoms_%dD_mpirank%04d_step%04d", dim,
+    cbvtk = GenericCallbacks.EveryXSimulationSteps(1) do (init=false)
+    mkpath("./vtk-bomex/")
+    outprefix = @sprintf("./vtk-bomex/dycoms_%dD_mpirank%04d_step%04d", dim,
                            MPI.Comm_rank(mpicomm), step[1])
     @debug "doing VTK output" outprefix
     writevtk(outprefix, Q, dg, flattenednames(vars_state(model,DT)), 
@@ -170,10 +214,10 @@ let
     SHF    = DT(15)
     C_drag = DT(0.0011)
     # Physical domain extents 
-    (xmin, xmax) = (0, 2000)
-    (ymin, ymax) = (0, 2000)
-    (zmin, zmax) = (0, 1500)
-    zsponge = DT(0.75 * zmax)
+    (xmin, xmax) = (0, 3000)
+    (ymin, ymax) = (0, 3000)
+    (zmin, zmax) = (0, 3000)
+    zsponge = DT(0.80 * zmax)
     #Get Nex, Ney from resolution
     Lx = xmax - xmin
     Ly = ymax - ymin
@@ -189,12 +233,11 @@ let
                   range(DT(zmin), length=Ne[3]+1, DT(zmax)))
     topl = StackedBrickTopology(mpicomm, brickrange,periodicity = (true, true, false), boundary=((0,0),(0,0),(1,2)))
     dt = 0.02
-    timeend = 100dt
+    timeend = dt
     dim = 3
     @info (ArrayType, DT, dim)
     result = run(mpicomm, ArrayType, dim, topl, 
                  polynomialorder, timeend, DT, dt, C_smag, LHF, SHF, C_drag, zmax, zsponge)
-    #@test result ≈ DT(0.9999737128867487)
   end
 end
 
