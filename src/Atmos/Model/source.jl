6086013f4afc2708f58546b1712343f5695a0268
asraero <asridhar@caltech.edu>
2019-10-07 13:59:03 -0700

    modified:   src/Atmos/Model/source.jl

--- src/Atmos/Model/source.jl
+++ src/Atmos/Model/source.jl
@@ -62,3 +62,47 @@ function atmos_source!(s::RayleighSponge, m::AtmosModel, source::Vars, state::Va
   end
   source.ρu -= state.ρu * coeff
 end
+
+
+"""
+  HeldSuarez{DT} <: Source
+"""
+struct HeldSuarez{DT} <: Source
+  σ_b::DT 
+  k_a::DT 
+  k_f::DT 
+  k_s::DT 
+  Δθz::DT 
+  ΔTy::DT 
+  p0::DT 
+  Ω::DT 
+  a_e::DT 
+end
+function atmos_source!(s::HeldSuarez, m::AtmosModel, source::Vars, state::Vars, aux::Vars, t::Real)
+  DT = eltype(state)
+  # coordinate transform
+  radius = sqrt(abs2,aux.coord)
+  # Spherical polars
+  @inbounds θ = atan(aux.coord[1], aux.coord[2])
+  @inbounds ϕ = asin(aux.coord[3] / radius)
+  ρ = state.ρ
+  ρu = state.ρu
+  # Assemble rotation matrix
+  # Don't need to do this at every point (or even every timestep -_- .. store in auxiliary or as mesh property at initialisation)
+  rotmat = SMatrix{3,3,DT}(sin(λ)*cos(ϕ), cos(θ)*cos(ϕ), -sin(ϕ), 
+                           sin(θ)*sin(ϕ), cos(θ)*sin(ϕ), cos(ϕ),
+                           -cos(θ), -sin(θ), 0)
+  vel_spherical = rotmat * state.ρu
+  z = aux.orientation.Φ / grav
+  Δσ = (σ-σ_b)/(1-σ_b)
+  E_int = state.ρ * internal_energy(state) 
+  p = air_pressure(state)
+  T_eq = max(200, (315-ΔTy*sin(ϕ)^2 - Δθz * log(p/p0)*cos(ϕ)^2)*(p/p0)^κ)
+  E_int_eq = state.ρ * internal_energy(T_eq)
+  k_v = k_f * max(0, Δσ)
+  k_T = k_a + (k_s - k_a)*max(0,Δσ)*(cos(ϕ))^4
+  # Project onto spherical space for latitude / longitudinal velocity conversion
+  # Cartesian to spherical projection functions. Probably define these in Mesh.Geometry (?)
+  source.ρu = - inv(rotmat)*SVector{3,DT}(0,-k_v * vel_spherical[2], 0)
+  source.ρe = -k_T * (E_int - E_int_eq) - k_v * ρu' * ρu/ρ
+end
